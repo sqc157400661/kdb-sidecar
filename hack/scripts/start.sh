f6635bca7dd33779bf3d7b8f300bc37aa7a158ce
sqc <sqc157400661@163.com>
2024-11-12 15:29:59 +0800

    commit dockerfile scripts

---
+++ hack/scripts/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# set default values
+Engine="MYSQL"
+ConfigFile="/kdb/conf/config.yaml"
+
+# update configuration file path based on environment variables
+if [ -n "${CONFIG_FILE_ENV}" ]; then
+    ConfigFile="${CONFIG_FILE_ENV}"
+fi
+
+# update database engine based on environment variables
+if [ -n "${ENGINE_ENV}" ]; then
+    Engine="${ENGINE_ENV}"
+fi
+
+if [[ "${Engine}" == "MYSQL" || "${Engine}" == "mysql" || "${Engine}" == "MySQL" ]]; then
+    /kdb/bin/manager MySQLSidecar -c "${ConfigFile}"
+else
+    /kdb/bin/manager PGSidecar -c "${ConfigFile}"
+fi
